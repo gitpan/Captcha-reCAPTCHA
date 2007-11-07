00a27ea80abf0a1cfd4c442ad2a97d653c868c5b
Andy Armstrong <andy@hexten.net>
2007-11-07 13:39:14 +0000

    Import of ANDYA/Captcha-reCAPTCHA-0.91 from CPAN.
    gitpan-cpan-distribution: Captcha-reCAPTCHA
    gitpan-cpan-version:      0.91
    gitpan-cpan-path:         ANDYA/Captcha-reCAPTCHA-0.91.tar.gz
    gitpan-cpan-author:       ANDYA
    gitpan-cpan-maturity:     released

--- lib/Captcha/reCAPTCHA/Mailhide.pm
+++ lib/Captcha/reCAPTCHA/Mailhide.pm
@@ -7,7 +7,7 @@ use Crypt::Rijndael;
 use MIME::Base64;
 use HTML::Tiny;
 
-use version; our $VERSION = qv( '0.9' );
+our $VERSION = '0.91';
 
 use constant API_MAILHIDE_SERVER => 'http://mailhide.recaptcha.net';
 
@@ -126,7 +126,7 @@ Captcha::reCAPTCHA::Mailhide - A Perl implementation of the reCAPTCHA Mailhide A
 
 =head1 VERSION
 
-This document describes Captcha::reCAPTCHA::Mailhide version 0.9
+This document describes Captcha::reCAPTCHA::Mailhide version 0.91
 
 =head1 SYNOPSIS
 
